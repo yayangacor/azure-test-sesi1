f04586617253a826698ab79abda0f592657f7191
yayangacor <149252963+yayangacor@users.noreply.github.com>
2026-09-24 23:02:33 +0700

    deployment testing

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
+app.listen(PORT, () =>{
+    console.log(`server sudah jalan di port ${PORT}`)
+})
